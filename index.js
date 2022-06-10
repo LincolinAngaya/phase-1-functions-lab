21ede72776a7860e1c6a636da29b889a71f5af82
LincolinAngaya <lincolinangaya5626@gmail.com>
2022-06-10 19:53:34 +0300

    commit tree 557401516b6b34f9d7c9f9d20d6a861ab319755a

--- index.js
+++ index.js
@@ -16,15 +16,18 @@ const distanceTravelledInFeet = (start, destination) => {
         distance;
 };
 
-const calculatesFarePrice = (start, destination) => {
+function calculatesFarePrice(start, destination) {
     let distanceTravelled = distanceTravelledInFeet(start, destination);
     if (distanceTravelled < 400) {
         return 0;
-    } else if (distanceTravelled > 400 && distanceTravelled <= 2000) {
-        return (distanceTravelled - 400) * 0.02;
-    } else if (distanceTravelled > 2000 && distanceTravelled <= 2500) {
+
+    } else if (distanceTravelled > 400) {
+        return (distanceTravelled - 400) * 0.02
+    } else if (distanceTravelled <= 2000) {
+        return (distanceTravelled - 400) * 0.02
+    } else if (distanceTravelled > 2000 && distanceTravelled < 2500) {
         return 25;
     } else {
         return "cannot travel that far";
     }
-};
+}
